ee00a52e8fffd3c1ccd0f23e6cec2d85a0b5e2ac
uzma <92790017+uzma-nazim@users.noreply.github.com>
2022-03-06 04:40:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let mobdiplay =document.getElementById("mobdiplay")
+const Toggle = ()=>{
+
+    mobdiplay.style.right = "0%"
+}
+const toggeleOff = ()=>{
+    mobdiplay.style.right = "-100%"
+
+
+}
